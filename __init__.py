62c8d260d3b334c24634d5720b055611bdda5719
DjLex2021 <91385866+DjLex2021@users.noreply.github.com>
2025-01-07 20:03:19 +0100

    Update __init__.py
    Add new async_forward_entry_setups

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-"""The sp108e_ws2815 integration."""
+"""The sp108e integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
@@ -7,17 +7,16 @@ PLATFORMS = ["light"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the sp108e_ws2815 component."""
+    """Set up the sp108e component."""
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up sp108e_ws2815 from a config entry."""
+    """Set up sp108e from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data.get("host")
 
-    for platform in PLATFORMS:
-        await hass.config_entries.async_forward_entry_setup(entry, platform)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
