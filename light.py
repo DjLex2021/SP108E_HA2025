4185b53de816eb64b66b564f93552ae6f9f4c0bf
DjLex2021 <91385866+DjLex2021@users.noreply.github.com>
2025-01-09 22:35:13 +0100

    register light in hass.data

--- light.py
+++ light.py
@@ -1,19 +1,25 @@
 from .pyledshop import WifiLedShopLight
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .const import DOMAIN
 import logging
 
 _LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up sp108e lights."""
     host = entry.data.get("host")
     name = entry.data.get("name")
-    
+
     _LOGGER.debug("Setting up SP108E entity for host: %s", entry.data.get("host"))
 
-    # Erstelle die Entität für das Licht
-    entity = WifiLedShopLight(host, name)
-    async_add_entities([entity], update_before_add=True)
+    # Erstelle die Instanz des Lichts
+    light = WifiLedShopLight(host, name)
+    
+    # Speichere die Instanz in hass.data, damit andere Plattformen darauf zugreifen können
+    hass.data[DOMAIN][entry.entry_id] = light
+
+    # Füge die Entität für das Licht hinzu
+    async_add_entities([light], update_before_add=True)
+
     return True
